8606a454b0300cfb6245c7d6da6529b6586e8f3a
hkj <hkj1984@gmail.com>
2022-06-07 23:29:11 +0900

    레이아웃 xml로 화면 구성하기

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,42 +1,13 @@
 package com.example.androidlab
 
-import android.graphics.Typeface
 import android.os.Bundle
-import android.view.Gravity
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 이름 문자열 출력 TextView 생성
-        val name = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "Lake Louise"
-        }
-        // 이미지 출력 ImageView 생성
-        val image = ImageView(this).also {
-            it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.lake_1))
-        }
-        // 주소 문자열 출력 TextView 생성
-        val address = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "lake Louise, AB, 캐나다"
-        }
-        // 뷰를 레이아웃에 배치
-        val layout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-            addView(name, WRAP_CONTENT, WRAP_CONTENT)
-            addView(image, WRAP_CONTENT, WRAP_CONTENT)
-            addView(address, WRAP_CONTENT, WRAP_CONTENT)
-        }
         // 레이아웃 출력
-        setContentView(layout)
+        setContentView(R.layout.activity_main)
     }
 }
